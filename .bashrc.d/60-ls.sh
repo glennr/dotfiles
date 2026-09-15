8bb25c70c6a84fcb45e7cd9afc1801b3109c1ced
Glenn Roberts <g@glenn-roberts.com>
2026-09-15 12:49:07 +1000

    ls alias

--- .bashrc.d/60-ls.sh
+++ .bashrc.d/60-ls.sh
@@ -1,4 +1,5 @@
 # Omarchy's ls is already `eza -lh`; ll adds dotfiles.
 alias ll='eza -lah --group-directories-first --icons=auto'
+alias l=ll
 # ls -lrt: newest at the bottom. eza's modified sort is oldest-first already.
 alias lrt='eza -lh --sort=modified --icons=auto'
